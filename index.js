ce639a0086c71898b165daeaec77bfcbc1c33954
tonyrpark <tonyrpark@gmail.com>
2021-02-17 12:02:27 -0800

    Created starting variables in index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+//Variable Declaration
+const inquirer = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+//generate.js is in separate folder
+const generate = require("./assets/generate");
